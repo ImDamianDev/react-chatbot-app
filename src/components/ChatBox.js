bf31f8c290c107e9e3843d15b64efbe418cfad5a
imdamiandev <imdamian.dev@gmail.com>
2023-03-27 12:07:45 -0300

    re style app

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -3,8 +3,9 @@ import "./ChatBox.css";
 import { GPTChat } from "../api/GPTChat";
 
 export const ChatBox = () => {
+
     const [chatHistory, setChatHistory] = useState([]);
-    const [inputText, setInputText] = useState("Conoces Chile");
+    const [inputText, setInputText] = useState("");
 
     console.log(chatHistory)
     const handleInputChange = (event) => {
@@ -28,7 +29,7 @@ export const ChatBox = () => {
                     timestamp: new Date().getTime(),
                 };
                 setChatHistory([...chatHistory, userMessage, botResponse]);
-                //setInputText("");
+                setInputText("");
             }
         }
     };
@@ -49,31 +50,48 @@ export const ChatBox = () => {
                 timestamp: new Date().getTime(),
             };
             setChatHistory([...chatHistory, userMessage, botResponse]);
-            //setInputText("");
+            setInputText("");
         }
     };
 
     return (
         <div className="chat-box-container">
-            <div className="chat-box">
+
+            <div className="chat-box mb-5">
+                <div
+                    className={`message bot`}
+                >
+                    <div className={`message-text bot`}>
+                        Hola! Soy una aplicacion creada con React que utiliza un modelo de lenguaje desarrollado por OpenAI.
+                    </div>
+                </div>
+                <div
+                    className={`message bot`}
+                >
+                    <div className={`message-text bot`}>
+                        Estoy diseñado para responder preguntas y mantener conversaciones en varios temas. ¿En qué puedo ayudarte hoy?
+                    </div>
+                </div>
                 {chatHistory.map((message, index) => (
                     <div
                         className={`message ${message.sender}`}
                         key={`${message.sender}-${message.timestamp}`}
                     >
                         <div className={`message-text ${message.sender}`}>
+                            <h5 className="sender-name">{message.sender}</h5>
                             {message.message}
                         </div>
                     </div>
                 ))}
             </div>
-            <div className="input-container">
+
+            <div className="input-container d-flex">
                 <input
                     type="text"
                     value={inputText}
                     onChange={handleInputChange}
                     onKeyDown={handleKeyDown}
-                    placeholder="Type your message here..."
+                    placeholder="Escribe tu pregunta aqui..."
                 />
                 <button onClick={handleSendClick}>Send</button>
             </div>
